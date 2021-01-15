2b59b6ec03c9c850bf2ffadf0ce0d84d0b7a6a16
Joseph Sabbagh <joseph544310@gmail.com>
2021-01-15 17:30:10 +0100

    create-react-app and folder structuring

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/App.css';
+import data from '../discovery_page.json'
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
